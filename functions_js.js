e637722852ca85f745b7bffee105b3c31545d77c
chidinma <cijeoma214@gmail.com>
2022-07-01 19:46:01 +0100

    Add files via upload

---
+++ functions_js.js
@@ -0,0 +1,27 @@
+function fun (){
+   alert('Hello welcome to todays tutorial');
+}
+fun();
+
+//a function that will take in a name and say hello
+function greeting()
+
+{
+let name=prompt('please what is your name?');
+
+let result="hello beautiful "+name + " is nice to meet you";
+
+console.log(result);
+}
+greeting();
+
+//function to take arguments
+let num1=10;
+let num2=20;
+let answer =num1+num2;
+function sumNumbers(num1,num2){
+
+
+
+console.log(answer);}
+sumNumbers();
